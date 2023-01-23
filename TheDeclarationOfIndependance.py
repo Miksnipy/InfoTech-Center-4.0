09835b6777fa028c17f299365756ff201490add2
Roman Cleary <romancleary0@gmail.com>
2023-01-23 13:49:40 -0800

    update welcome message

--- TheDeclarationOfIndependance.py
+++ TheDeclarationOfIndependance.py
@@ -1,5 +1,12 @@
 # programmer: Roman Cleary
 # date:1.20.2023
 # Program: Infotech center upgrades
+"""
+Our welcome screen will start our program letting
+drivers know that the InfoTech Center 4.0 OS is loading
+"""
 
 
+print("\n\nWelcome - Infotech Center 4.0")
+
+print("\nInfotech Center 4.0 OS is loading")
