f0f8508cd5687dd59d01eb769786dfafd0bb1364
Roman Cleary <romancleary0@gmail.com>
2023-01-23 14:13:38 -0800

    add time.sleep

--- TheDeclarationOfIndependance.py
+++ TheDeclarationOfIndependance.py
@@ -1,5 +1,17 @@
 # programmer: Roman Cleary
 # date:1.20.2023
 # Program: Infotech center upgrades
+"""
+Our welcome screen will start our program letting
+drivers know that the InfoTech Center 4.0 OS is loading
+"""
+#Import Libraries Here
+from time import sleep
+x=2
 
+
+print("\n\nWelcome - Infotech Center 4.0")
+time.sleep(x)
+
+print("\nInfotech Center 4.0 OS is loading")
 
