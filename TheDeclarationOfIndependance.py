62665b563b288a088e8c1f7fe642e23978e21177
Roman Cleary <romancleary0@gmail.com>
2023-01-24 14:08:15 -0800

    put in loading  screen

--- TheDeclarationOfIndependance.py
+++ TheDeclarationOfIndependance.py
@@ -5,13 +5,22 @@
 Our welcome screen will start our program letting
 drivers know that the InfoTech Center 4.0 OS is loading
 """
-#Import Libraries Here
-from time import sleep
+import time
+import sys
 x=2
-
+a=0
 
 print("\n\nWelcome - Infotech Center 4.0")
 time.sleep(x)
 
-print("\nInfotech Center 4.0 OS is loading")
 
+while x != 20:
+    x += 1
+    b = ("\033[1;33;40m InfoTech Center OS is Loading" + "." * a)
+    a = a + 1
+    sys.stdout.write('\r'+b) # \r prints a carriage return first, so `b` is printed on top of the previous line.
+    time.sleep(0.5)
+    if a == 4:
+        a = 0
+    if x == 20:
+        print('\033[1;32;40m Done!')
