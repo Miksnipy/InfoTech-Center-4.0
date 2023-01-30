11263753c9094cc9eb52be29e09ab488bbe43bc2
Roman Cleary <romancleary0@gmail.com>
2023-01-30 13:32:05 -0800

    changed color  and fixed spacing

--- TheDeclarationOfIndependance.py
+++ TheDeclarationOfIndependance.py
@@ -10,17 +10,17 @@ import sys
 x=2
 a=0
 
-print("\n\nWelcome - Infotech Center 4.0")
+print("\n\n\033[1;33;40mWelcome - Infotech Center 4.0\n")
 time.sleep(x)
 
 
 while x != 20:
     x += 1
-    b = ("\033[1;33;40m InfoTech Center OS is Loading" + "." * a)
+    b = ("\033[1;33;40mInfoTech Center OS is Loading" + "." * a)
     a = a + 1
     sys.stdout.write('\r'+b) # \r prints a carriage return first, so `b` is printed on top of the previous line.
     time.sleep(0.5)
     if a == 4:
         a = 0
     if x == 20:
-        print('\033[1;32;40m Done!')
+        print('\n\033[1;32;40mDone!')
