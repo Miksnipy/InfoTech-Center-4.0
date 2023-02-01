2da0737d217628076b029c3611a7f01c0a133789
Roman Cleary <romancleary0@gmail.com>
2023-02-01 14:08:17 -0800

    add gas level alert function wip

--- TheDeclarationOfIndependance.py
+++ TheDeclarationOfIndependance.py
@@ -19,21 +19,43 @@ Create a Function to determine our gas levels and closest gas station
 
 # Import Libraries Here
 import random
-
+from time import sleep
 
 # Gas Level Function
 def gasLevelGauge():
     gasLevelList = ["Empty","Low","Quarter Tank","Half Tank","Three Quarter Tank","Full Tank"]
 
     currentGasLevel = random.choice(gasLevelList)
-    return currentGasLeval
-gasLevelGauge()
+    return currentGasLevel
 
+# Variable calling gasLevelGuage function to store its value
+gasLevelIndicator = gasLevelGauge()
+
+# List of Gas Stations Function
 def listOfGasStation():
     gasStations = ["Shell","Costco","Buckeys","Speedway","7-11","Circkle-k","Meijer","Family Fare"]
     gasStationNearby = random.choice(gasStations)
     return gasStationNearby
-listOfGasStation()
+# determine gas level & Closest gas station
+
+
+def gasLevelAlert():
+
+
+    milesToGasStationLow = round(random.uniform(1, 25), 2)
+    milesToGasStationQuartTank = round(random.uniform(26, 50), 2)
+    if gasLevelIndicator == "Empty":
+        print("***WARNING YOU ARE ON EMPTY***")
+        sleep(1)
+        print("Calling Emergency Contact")
+    elif gasLevelIndicator == "Low":
+        print("****Warning*****")
+        sleep(1)
+        print("Your Gas Level is low checking Google Maps for the closest gas station.")
+        sleep(1.5)
+        print("The closest gas station is",listOfGasStation(),"which is",milesToGasStationLow, "miles away")
+gasLevelAlert()
+
 
 
 
