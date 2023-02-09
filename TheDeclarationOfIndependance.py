af2fd2040fb45f0d48ba5057114b2654d9f1aef3
Roman Cleary <romancleary0@gmail.com>
2023-02-09 13:52:59 -0800

    git commit -m "Defined Weather Function and called - Stable

--- TheDeclarationOfIndependance.py
+++ TheDeclarationOfIndependance.py
@@ -2,4 +2,20 @@
 # date:2.8.2023
 # Program: Weather System Updates
 
+#Import Libraries here
+import random
 
+#Create weather condition in a list and choose it randomly
+def weather():
+	weatherForcast = ["Snowing", "Blizerd","Raining","Foggy","Windy","Icy", "Sunshine"]
+	weatherCondiction = random.choice(weatherForcast)
+	return weatherCondiction
+
+#vaeibal to call weather() once in our VRS()
+weatherAlert = weather()
+
+print(weatherAlert)
+
+#VRS() to respond to weather condition
+def vehicleResponseSystem():
+	print("hellllllllllllllllllooooooooooooooooowwwwwwwwwwwwwwww")
