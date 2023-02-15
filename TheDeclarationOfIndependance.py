928e1c61cc36542995a80b2200e2c46667aa3f9d
Roman Cleary <romancleary0@gmail.com>
2023-02-15 13:01:41 -0800

    2/15/2023

--- TheDeclarationOfIndependance.py
+++ TheDeclarationOfIndependance.py
@@ -1,5 +1,30 @@
-# programmer: Roman Cleary
-# date:1.20.2023
-# Program: Infotech center upgrades
+# Programmer: Roman Cleary
+# Date:2.15.2023
+# Program: Infotech center 4.0 -Restaurants
+
+
+
+"""
+We will create a function that will tell us the closest Restaurant
+    - create a list of Restaurant
+"""
+
+
+# Gas Level Function
+def gasLevelGauge():
+    gasLevelList = ["Quarter Tank","Half Tank","Three Quarter Tank","Full Tank"]
+
+    currentGasLevel = random.choice(gasLevelList)
+    return currentGasLevel
+
+# Variable calling gasLevelGuage function to store its value
+gasLevelIndicator = gasLevelGauge()
+
+
+
+
+
+
+
 
 
