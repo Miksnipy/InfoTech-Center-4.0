e029602a9dced9e9273bd31c2a8bbd34187e8a28
Roman Cleary <romancleary0@gmail.com>
2023-03-01 13:17:40 -0800

    fixed code

--- TheDeclarationOfIndependance.py
+++ TheDeclarationOfIndependance.py
@@ -115,9 +115,9 @@ def gasLevelAlert():
 
 #Create weather condition in a list and choose it randomly
 def weather():
-	weatherForcast = ["Snowing", "Blizerd","Rain","Foggy","Windy","Icy", "Sunshine"]
-	weatherCondiction = random.choice(weatherForcast)
-	return weatherCondiction
+    weatherForcast = ["Snowing", "Blizerd","Rain","Foggy","Windy","Icy", "Sunshine"]
+    weatherCondiction = random.choice(weatherForcast)
+    return weatherCondiction
 
 #vaeibal to call weather() once in our VRS()
 weatherAlert = weather()
@@ -128,30 +128,45 @@ weatherAlert = weather()
 
 #VRS() to respond to weather condition
 def vehicleResponseSystem():
-	if weatherAlert == "Snowing":
-		print("\nNWS has changed your alarm by 15 minutes because of the waether forcast of", weatherAlert)
-		print("\nVRS has been engaged only allowing your vehicle to go 45 MPH")
-	elif weatherAlert == "Blizerd":
-		print("\nNWS has changed your alarm by 30 minutes because of the waether forcast of", weatherAlert)
-		print("\nVRS has been engaged only allowing your vehicle to go 35 MPH")
-	elif weatherAlert == "Rain":
-		print("\nNWS has not changed your alarm ", weatherAlert)
-		print("\nVRS has not been engaged ")
-	elif weatherAlert == "Foggy":
-		print("\nNWS has not changed your alarm ", weatherAlert)
-		print("\nVRS has not been engaged ")
-	elif weatherAlert == "Windy":
-		print("\nNWS has not changed your alarm ", weatherAlert)
-		print("\nVRS has not been engaged")
-	elif weatherAlert == "Icy":
-		print("\nNWS has changed your alarm by 45 minutes because of the waether forcast of", weatherAlert)
-		print("\nVRS has been engaged only allowing your vehicle to go 25 MPH")
-	else:
-		print("\nNWS has not changed your alarm ", weatherAlert)
-		print("\nVRS has not been engaged ")
+    if weatherAlert == "Snowing":
+        print("\nNWS has changed your alarm by 15 minutes because of the waether forcast of", weatherAlert)
+        print("\nVRS has been engaged only allowing your vehicle to go 45 MPH")
+    elif weatherAlert == "Blizerd":
+        print("\nNWS has changed your alarm by 30 minutes because of the waether forcast of", weatherAlert)
+        print("\nVRS has been engaged only allowing your vehicle to go 35 MPH")
+    elif weatherAlert == "Rain":
+        print("\nNWS has not changed your alarm ", weatherAlert)
+        print("\nVRS has not been engaged ")
+    elif weatherAlert == "Foggy":
+        print("\nNWS has not changed your alarm ", weatherAlert)
+        print("\nVRS has not been engaged ")
+    elif weatherAlert == "Windy":
+        print("\nNWS has not changed your alarm ", weatherAlert)
+        print("\nVRS has not been engaged")
+    elif weatherAlert == "Icy":
+        print("\nNWS has changed your alarm by 45 minutes because of the waether forcast of", weatherAlert)
+        print("\nVRS has been engaged only allowing your vehicle to go 25 MPH")
+    else:
+        print("\nNWS has not changed your alarm ", weatherAlert)
+        print("\nVRS has not been engaged ")
+
+#function for restaurants
+def listOfRestaurants():
+    restaurants = ["Applebee", "Outback", "Red Lobster", "Denny's", "Cracker Barrel", "Wendys", "KFC", "Subway", "Burger Kings", "Arbys"]
+    restaurantsNearby = random.choice(restaurants)
+    return restaurantsNearby
 
 
+restaurantList = listOfRestaurants()
 
+
+def restaurantsfood():
+    milesToRestaurants = round(random.uniform(26, 50), 2)
+
+
+if restaurantList == "Applebee":
+    print("\nThis is a close Restaurant", listOfRestaurants(), "which is", restaurantsfood(), "miles away.")
+    sleep(1.5)
 
 #Call Functions here
 print("\nNational Weather survice is checking condiction")
